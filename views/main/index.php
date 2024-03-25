ab269f490f556b0a93609f986e475869b4ce627d
AlexanderA31 <ufredoalegria@hotmail.com>
2024-03-24 23:01:07 -0500

    Slider

--- views/main/index.php
+++ views/main/index.php
@@ -61,6 +61,87 @@
 <script> </script>
 	
 
+
+	<!-- Slider -->
+	<section class="section-slide">
+		<div class="wrap-slick1 rs2-slick1">
+			<div class="slick1">
+				<div class="item-slick1 bg-overlay1" style="background-color: #f0fff0; background-image: url(../../public/images/slide-04.jpg);" data-thumb="../../public/images/thumb-01.jpg" data-caption="Mujer">
+					<div class="container h-full">
+						<div class="flex-col-c-m h-full p-t-100 p-b-60 respon5">
+							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
+								<span class="ltext-202 txt-center cl0 respon2">
+									Nueva ropa para mujer
+								</span>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
+								<h2 class="ltext-104 txt-center cl0 p-t-22 p-b-40 respon1">
+									Frescura a cada paso
+								</h2>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
+								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn2 p-lr-15 trans-04">
+									Entrar
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="item-slick1 bg-overlay1" style="background-color: #f0fff0; background-image: url(../../public/images/slide-06.jpg);" data-thumb="../../public/images/thumb-02.jpg" data-caption="Niño">
+					<div class="container h-full">
+						<div class="flex-col-c-m h-full p-t-100 p-b-60 respon5">
+							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
+								<span class="ltext-202 txt-center cl0 respon2">
+									Nueva ropa de niño
+								</span>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
+								<h2 class="ltext-104 txt-center cl0 p-t-22 p-b-40 respon1">
+								   Por que nos preocupamos por ellos
+								</h2>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
+								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn2 p-lr-15 trans-04">
+									Entrar
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="item-slick1 bg-overlay1" style="background-color: #f0fff0; background-image: url(../../public/images/slide-07.jpg);" data-thumb="../../public/images/thumb-03.jpg" data-caption="Hombre">
+					<div class="container h-full">
+						<div class="flex-col-c-m h-full p-t-100 p-b-60 respon5">
+							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
+								<span class="ltext-202 txt-center cl0 respon2">
+									Ropa de hombre
+								</span>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
+								<h2 class="ltext-104 txt-center cl0 p-t-22 p-b-40 respon1">
+									Todo lo nuevo por Asotaeco
+								</h2>
+							</div>
+
+							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
+								<a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn2 p-lr-15 trans-04">
+									Entrar
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="wrap-slick1-dots p-lr-10"></div>
+		</div>
+	</section>
 	<!-- <div class="item-slick1" style="background-image: url(../../public/images/slide-03.jpg);">
 			<div class="container h-full">
 				<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
@@ -102,7 +183,7 @@
 				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
 					<!-- Block1 -->
 					<div class="block1 wrap-pic-w">
-						<img src="../../public/images/banner-02.jpg" alt="IMG-BANNER">
+						<img src="categoriaimg/estudiantes.png" alt="IMG-BANNER" style="height:100px; weight:150px;">
 
 						<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
