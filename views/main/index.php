ed9caaf14d5df88e4cea67a2878a52c679daa7da
AlexanderA31 <ufredoalegria@hotmail.com>
2024-03-26 19:56:15 -0500

    Img

--- views/main/index.php
+++ views/main/index.php
@@ -188,10 +188,12 @@ if ($_SESSION['user_session']['rol_id'] == "1") {
 
 			<div class="container">
 				<div class="row">
+
+
 					<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
 						<!-- Block1 -->
 						<div class="block1 wrap-pic-w">
-							<img src="categoriaimg/estudiantes.png" alt="IMG-BANNER" style="height:100px; weight:150px;">
+							<img src="../../public/images/banner-01.jpg" alt="IMG-BANNER">
 
 							<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 								<div class="block1-txt-child1 flex-col-l">
@@ -267,7 +269,7 @@ if ($_SESSION['user_session']['rol_id'] == "1") {
 					<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
 						<!-- Block1 -->
 						<div class="block1 wrap-pic-w">
-							<img src="../../public/images/banner-02.jpg" alt="IMG-BANNER">
+							<img src="../../public/images/banner-04.jpg" alt="IMG-BANNER">
 
 							<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 								<div class="block1-txt-child1 flex-col-l">
@@ -294,7 +296,7 @@ if ($_SESSION['user_session']['rol_id'] == "1") {
 					<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
 						<!-- Block1 -->
 						<div class="block1 wrap-pic-w">
-							<img src="../../public/images/banner-02.jpg" alt="IMG-BANNER">
+							<img src="../../public/images/banner-05.jpg" alt="IMG-BANNER">
 
 							<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 								<div class="block1-txt-child1 flex-col-l">
