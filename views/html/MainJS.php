8719b76f8f051c56f433dd9a8b64ec245809cb34
Alexander-Luna <paulluna99@gmail.com>
2024-03-27 23:45:45 -0500

    WhatsApp

--- views/html/MainJS.php
+++ views/html/MainJS.php
@@ -1,26 +1,26 @@
 <!--===============================================================================================-->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!-- <script src="../../public/vendor/jquery/jquery-3.2.1.min.js"></script> -->
-<script src="../../public/vendor/isotope/isotope.pkgd.min.js"></script>
+<!-- <script src="../../vendor/jquery/jquery-3.2.1.min.js"></script> -->
+<script src="../../vendor/isotope/isotope.pkgd.min.js"></script>
 
 <!--===============================================================================================-->
-<script src="../../public/vendor/animsition/js/animsition.min.js"></script>
+<script src="../../vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-<script src="../../public/vendor/bootstrap/js/popper.js"></script>
-<script src="../../public/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../vendor/bootstrap/js/popper.js"></script>
+<script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-<script src="../../public/vendor/select2/select2.min.js"></script>
+<script src="../../vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-<script src="../../public/vendor/daterangepicker/moment.min.js"></script>
-<script src="../../public/vendor/daterangepicker/daterangepicker.js"></script>
+<script src="../../vendor/daterangepicker/moment.min.js"></script>
+<script src="../../vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-<script src="../../public/vendor/slick/slick.min.js"></script>
+<script src="../../vendor/slick/slick.min.js"></script>
 <!--===============================================================================================-->
-<script src="../../public/vendor/parallax100/parallax100.js"></script>
-<script src="../../public/vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
-<script src="../../public/vendor/sweetalert/sweetalert.min.js"></script>
-<script src="../../public/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="../../vendor/parallax100/parallax100.js"></script>
+<script src="../../vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
+<script src="../../vendor/sweetalert/sweetalert.min.js"></script>
+<script src="../../vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="../../public/js/slick-custom.js"></script>
 <script src="../../public/js/main.js"></script>
 
