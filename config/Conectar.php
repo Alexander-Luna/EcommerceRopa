358764a029327a25a5faea6dd34694d323a273ee
Alexander-Luna <paulluna99@gmail.com>
2024-03-27 22:00:32 -0500

    Prueba2

--- config/Conectar.php
+++ config/Conectar.php
@@ -9,11 +9,12 @@ class Conectar
 
     protected function Conexion()
     {
-        $dsn = "mysql:host=localhost;dbname=ecommerce1;charset=utf8";
+        $dsn = "mysql:host=localhost;dbname=u823153798_ecomerce1;charset=utf8";
 
         $credentials = [
             ["username" => "dark", "password" => "12345"],
-            ["username" => "root", "password" => "123456"]
+            ["username" => "root", "password" => "123456"],
+            ["username" => "u823153798_dark", "password" => "QPiRidRPi|0"]
         ];
 
         foreach ($credentials as $credential) {
