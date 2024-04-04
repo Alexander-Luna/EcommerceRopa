fbdd8cd95c1bc81a5b060ec1e772ce58b6930a23
Alexander-Luna <paulluna99@gmail.com>
2024-04-04 02:16:24 -0500

    Shop

--- views/shop/index.php
+++ views/shop/index.php
@@ -21,7 +21,7 @@
 			<div class="flex-w flex-sb-m p-b-52">
 				<div class="flex-w flex-l-m filter-tope-group m-tb-10">
 					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
-						Todos lso Productos
+						Todos los Productos
 					</button>
 
 					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".mujer">
@@ -32,8 +32,11 @@
 						Hombres
 					</button>
 
-					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".ninio">
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".niño">
 						Niños
+					</button>
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".niña">
+						Niñas
 					</button>
 				</div>
 
