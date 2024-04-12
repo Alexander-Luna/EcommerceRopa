2eb327b07e96e981e50810e2eb4825ed1e8b393b
Alexander-Luna <paulluna99@gmail.com>
2024-04-12 14:38:09 -0500

    Email8

--- controllers/SMTPemail.php
+++ controllers/SMTPemail.php
@@ -9,34 +9,59 @@ use PHPMailer\PHPMailer\Exception;
 
 class SmtpModel extends Conectar
 {
-    private $mail;
 
-    public function __construct()
-    {
-        $this->mail = new PHPMailer(true);
-        $this->mail->isSMTP();
-        $this->mail->Host = 'smtp.hostinger.com';
-        $this->mail->SMTPAuth = true;
-        $this->mail->Username = 'info@asotaeco.com';
-        $this->mail->Password = 'Asotaeco1@';
-        $this->mail->SMTPSecure = 'ssl';
-        $this->mail->Port = 465;
-    }
 
     public function enviarCorreo($email, $nombre, $asunto, $body)
     {
         try {
-            $this->mail->setFrom('info@asotaeco.com', 'Asotaeco');
-            $this->mail->addAddress($email, $nombre);
-            $this->mail->isHTML(true);
-            $this->mail->Subject = $asunto;
-            $this->mail->Body = $body;
-            $this->mail->AltBody = strip_tags($body);
-            $rta = $this->mail->send();
+            // $mailer = new PHPMailer(true);
+            // $mailer->isSMTP();
+            // $mailer->Host = 'smtp.hostinger.com';
+            // $mailer->SMTPAuth = true;
+            // $mailer->Username = 'info@asotaeco.com';
+            // $mailer->Password = 'Asotaeco1@';
+            // $mailer->SMTPSecure = 'ssl';
+            // $mailer->Port = 465;
+
+            // $nombre = $_POST['nombre'];
+            // $apellido = $_POST['apellido'];
+            // $email = $_POST['email'];
+            // $asunto = $_POST['asunto'];
+            // $mensaje = $_POST['mensaje'];
+
+            $nombre = "Alexander";
+            $apellido = "Luna";
+            $email = "paulluna99@gmail.com";
+            $asunto = "asunto";
+            $mensaje = "asdasdasdasdasd";
+            // $foto = $_FILES['foto']; //array assoc - $foto['tmp_name']; $foto['size'] - $foto['name']
+            $foto = "";
+            if (empty(trim($nombre))) $nombre = 'anonimo';
+            if (empty(trim($apellido))) $apellido = '';
+
+            $body = <<<HTML
+    <h1>Contacto desde la web</h1>
+    <p>De: $nombre $apellido / $email</p>
+    <h2>Mensaje</h2>
+    $mensaje
+HTML;
+
+            $mailer = new PHPMailer(true);
+            $mailer->setFrom($email, "$nombre $apellido");
+            $mailer->addAddress('info@asotaeco.com', 'Sitio web');
+            $mailer->Subject = "Mensaje web: $asunto";
+            $mailer->msgHTML($body);
+            $mailer->AltBody = strip_tags($body);
+            $mailer->CharSet = 'UTF-8';
+
+            if ($foto['size'] > 0) {
+                $mailer->addAttachment($foto['tmp_name'], $foto['name']);
+            }
+
+            $rta =  $mailer->send();
             return $rta;
         } catch (Exception $e) {
             return false;
         }
     }
 }
-?>
